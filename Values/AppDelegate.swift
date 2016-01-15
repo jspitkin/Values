cdd77a2c124f61d706348e638186c006fb7b388f
Jake Pitkin <jakepitkin@gmail.com>
2016-01-15 11:21:36 -0700

    Simple arithmetic operations done

--- Values/AppDelegate.swift
+++ Values/AppDelegate.swift
@@ -12,11 +12,94 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var xSlider: UISlider?
+    var ySlider: UISlider?
+    var xValue: UILabel?
+    var yValue: UILabel?
+    var xLabel: UILabel?
+    var yLabel: UILabel?
+    
+    var addition: UILabel?
+    var subtraction: UILabel?
+    var multiplication: UILabel?
+    var division: UILabel?
+    var modulo: UILabel?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch. 
+
+        // A view controller contains a view
+        let rootViewController: UIViewController = UIViewController()
+        rootViewController.view.backgroundColor = UIColor.grayColor()
+        
+        // CGRectMake x pos, y pos, x size, y size
+        xSlider = UISlider(frame: CGRectMake(65, 100, 180, 30))
+        xValue = UILabel(frame: CGRectMake(260, 100, 50, 30))
+        xLabel = UILabel(frame: CGRectMake(40, 100, 200, 30))
+        xSlider?.addTarget(self, action: "sliderChanged", forControlEvents: UIControlEvents.ValueChanged)
+        rootViewController.view.addSubview(xValue!)
+        rootViewController.view.addSubview(xSlider!)
+        rootViewController.view.addSubview(xLabel!)
+        
+        ySlider = UISlider(frame: CGRectMake(65, 160, 180, 30))
+        yValue = UILabel(frame: CGRectMake(260, 160, 50, 30))
+        yLabel = UILabel(frame: CGRectMake(40, 160, 200, 30))
+        ySlider?.addTarget(self, action: "sliderChanged", forControlEvents: UIControlEvents.ValueChanged)
+        rootViewController.view.addSubview(yValue!)
+        rootViewController.view.addSubview(ySlider!)
+        rootViewController.view.addSubview(yLabel!)
+        
+        addition = UILabel(frame: CGRectMake(130, 220, 200, 30))
+        subtraction = UILabel(frame: CGRectMake(130, 260, 200, 30))
+        multiplication = UILabel(frame: CGRectMake(130, 300, 200, 30))
+        division = UILabel(frame: CGRectMake(130, 340, 200, 30))
+        modulo = UILabel(frame: CGRectMake(130, 380, 200, 30))
+        rootViewController.view.addSubview(addition!)
+        rootViewController.view.addSubview(subtraction!)
+        rootViewController.view.addSubview(multiplication!)
+        rootViewController.view.addSubview(division!)
+        rootViewController.view.addSubview(modulo!)
+        
+        xSlider?.minimumValue = 0
+        xSlider?.maximumValue = 100
+        xSlider?.value = 50
+        xLabel!.text = "X"
+        
+        ySlider?.minimumValue = 0
+        ySlider?.maximumValue = 100
+        ySlider?.value = 50
+        yLabel!.text = "Y"
+
+        sliderChanged()
+        
+        window = UIWindow()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        
         return true
+    }
+    
+    func sliderChanged() {
+        let xSliderValue: Int = Int(xSlider!.value)
+        let ySliderValue: Int = Int(ySlider!.value)
+        let sum: Int = xSliderValue + ySliderValue
+        let difference: Int = xSliderValue - ySliderValue
+        let product: Int = xSliderValue * ySliderValue
+        let quotient: Double = Double(xSliderValue) / Double(ySliderValue)
+        let modulus: Int = xSliderValue % ySliderValue
+        
+        xValue!.text = "\(xSliderValue)"
+        yValue!.text = "\(ySliderValue)"
+        addition!.text = "X + Y: \(sum)"
+        subtraction!.text = "X - Y: \(difference)"
+        multiplication!.text = "X * Y: \(product)"
+        if (ySliderValue == 0) {
+            division!.text = "X / Y: Undefined"
+        }
+        else {
+            division!.text = "X / Y: \(Double(round(10000*quotient)/10000))"
+        }
+        modulo!.text = "X % Y: \(modulus)"
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
